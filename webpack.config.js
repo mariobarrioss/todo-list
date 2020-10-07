b632cfc7de0ecf3b7d9758d74ac89b573b377100
Mario Barrios <mariobarriossalazar@outlook.com>
2020-10-07 10:21:58 -0600

    Configure webpack

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+
+module.exports = {
+  entry: {
+    app: './src/index.js',
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader',
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              bypassOnDebug: true,
+              disable: true,
+              mozjpeg: {
+                progressive: true,
+              },
+            },
+          },
+        ],
+      },
+    ],
+  },
+};
