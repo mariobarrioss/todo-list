2e1b0cc967a9d0d56e5e2e1b43b061c7daad03f9
Mario Barrios <mariobarriossalazar@outlook.com>
2021-01-11 11:04:52 -0600

    Add tests for retrieveProjects, updateProject and deleteProject in localStorage module

--- src/__tests__/localStorage.test.js
+++ src/__tests__/localStorage.test.js
@@ -1,4 +1,9 @@
-import { addProject, retrieveProjects } from '../localStorage';
+import {
+  addProject,
+  retrieveProjects,
+  updateProject,
+  deleteProject,
+} from '../localStorage';
 import Project from '../project';
 
 test('addProject should add new project object to array', () => {
@@ -6,4 +11,24 @@ test('addProject should add new project object to array', () => {
   const newProject = new Project('test project');
   addProject(newProject);
   expect(projectList[1]).toEqual({ name: 'test project', todos: [] });
+});
+
+test('retrieveProjects should return the projectList array with project objects', () => {
+  const projectList = retrieveProjects();
+  expect(projectList).toEqual([{ name: 'default', todos: [] }, { name: 'test project', todos: [] }]);
+});
+
+test('updateProject should take a project object and replace and object within projectList', () => {
+  const projectList = retrieveProjects();
+  const updatedProject = new Project('updated project');
+  updateProject(1, updatedProject);
+  expect(projectList[1]).toEqual({ name: 'updated project', todos: [] });
+});
+
+test('deleteProject should delete a project at the specified location', () => {
+  const projectList = retrieveProjects();
+  const lengthBeforeDeletion = projectList.length;
+  deleteProject(1);
+  expect(lengthBeforeDeletion).toBe(2);
+  expect(projectList.length).toBe(1);
 });
