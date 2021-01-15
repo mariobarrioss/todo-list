9e211dce3d230b1bd7c1bf5be2c5cfe4c72c95a0
rsundar <rohan.s.1987@gmail.com>
2021-01-15 20:52:26 +0530

    added todo tests

---
+++ src/__tests__/todo.test.js
@@ -0,0 +1,32 @@
+import Todo from '../todo';
+
+test('expect the todo object to br of type todo', () => {
+  const newTodo = new Todo('todo');
+  const t = newTodo instanceof Todo;
+  expect(t).toEqual(true);
+});
+
+test('testing update title function', () => {
+  const newTodo = new Todo();
+  newTodo.updateTitle('todo');
+  expect(newTodo.title).toEqual('todo');
+});
+
+test('testing update description function', () => {
+  const newTodo = new Todo();
+  newTodo.updateDescription("description");
+  expect(newTodo.description).toEqual('description');
+});
+
+test('testing update duedate function', () => {
+  const newTodo = new Todo();
+  newTodo.updateDueDate('20/12/2020');
+  expect(newTodo.dueDate).toEqual('20/12/2020');
+});
+
+test('testing update priority function', () => {
+  const newTodo = new Todo();
+  newTodo.updatePriority('medium');
+  expect(newTodo.priority).toEqual('medium');
+});
+
